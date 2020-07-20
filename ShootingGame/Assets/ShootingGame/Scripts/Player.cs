490fb46266cd11391b848ee4de9e9c60fe7cad7a
AnHongJae <sherlook369@naver.com>
2020-07-20 21:08:59 +0900

    슈팅게임 1일차

---
+++ ShootingGame/Assets/ShootingGame/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float m_Speed = 10f;
+
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+
+        Vector3 velocity = (new Vector3(xMove,yMove, 0).normalized * m_Speed);
+
+        Vector3 movement = velocity * Time.deltaTime;
+        transform.position = transform.position + movement;
+
+            
+    }
+
+
+
+
+
+
+}
