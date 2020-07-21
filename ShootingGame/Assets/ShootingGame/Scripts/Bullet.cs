b9ec1a3948c07b87a4a36e66cd84ae0ad69f3178
AnHongJae <sherlook369@naver.com>
2020-07-21 21:23:50 +0900

    슈팅게임 2일차

--- ShootingGame/Assets/ShootingGame/Scripts/Bullet.cs
+++ ShootingGame/Assets/ShootingGame/Scripts/Bullet.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public float m_Speed = 2f;
 
+    public float DeathTime = 5f;
+
     private void Update()
     {
         Vector3 movement = transform.up * m_Speed * Time.deltaTime;
         transform.position += movement;
+
+        DeathTime += Time.deltaTime;
+
+        if (DeathTime >= 2)
+        {
+            Destroy(gameObject);
+            DeathTime = 0;
+        }
 
     }
 }
--- ShootingGame/Assets/ShootingGame/Scripts/Player.cs
+++ ShootingGame/Assets/ShootingGame/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float m_Speed = 10f;
+    public GameObject m_Bullet;
+    public float CreatTime = 0f;
+
 
     void Update()
     {
@@ -17,7 +20,26 @@ public class Player : MonoBehaviour
         Vector3 movement = velocity * Time.deltaTime;
         transform.position = transform.position + movement;
 
-            
+
+        
+
+        CreatTime += Time.deltaTime;
+        if(CreatTime>0.5 && Input.GetKey(KeyCode.Space))
+        {
+            var gabj = GameObject.Instantiate(m_Bullet);
+            gabj.transform.position = transform.position + new Vector3(0, 4, 0);
+
+            var gabj2 = GameObject.Instantiate(m_Bullet);
+            gabj2.transform.position = transform.position + new Vector3(-1, 4, 0);
+            gabj2.transform.eulerAngles = transform.eulerAngles + new Vector3(0,4,25); 
+
+            var gabj3 = GameObject.Instantiate(m_Bullet);
+            gabj3.transform.position = transform.position + new Vector3(1, 4, 0);
+            gabj3.transform.eulerAngles = transform.eulerAngles + new Vector3(1, 4, -25);
+
+            CreatTime = 0;
+
+        }
     }
 
 
